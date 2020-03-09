4ef45065fe3f78909bfaeca6ade32b88b0b13733
jordantoaster <jordanmcdonaldmain@gmail.com>
2020-03-09 18:47:15 +0000

    WIP glue initial s3 interaction

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,17 +3,59 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "incoming_data_bucket" {
-  bucket = "jm_etl_incoming_data"
+  bucket = "jm-etl-incoming-data"
   acl = "private"
 }
 
 resource "aws_s3_bucket" "transformed_data_bucket" {
-  bucket = "jm_etl_transformed_data"
+  bucket = "jm-etl-transformed-data"
   acl = "private"
 }
 
-resource "aws_iam_service_linked_role" "glue_service_role" {
-  aws_service_name = "aws-glue.amazonaws.com"
+resource "aws_iam_role" "glue" {
+  name = "AWSGlueServiceRoleDefault"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "glue.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "glue_service" {
+    role = "${aws_iam_role.glue.id}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy" "my_s3_policy" {
+  name = "my_s3_policy"
+  role = "${aws_iam_role.glue.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:*"
+      ],
+      "Resource": [
+        "arn:aws:s3:::my_bucket",
+        "arn:aws:s3:::my_bucket/*"
+      ]
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_glue_catalog_database" "aws_glue_catalog_database_incoming" {
@@ -23,7 +65,7 @@ resource "aws_glue_catalog_database" "aws_glue_catalog_database_incoming" {
 resource "aws_glue_crawler" "incoming_data_bucket_glue_crawler" {
   database_name = "${aws_glue_catalog_database.aws_glue_catalog_database_incoming.name}"
   name = "incoming_data_crawler"
-  role = "${aws_iam_service_linked_role.glue_service_role.aws_service_name}"
+  role = "${aws_iam_role.glue.arn}"
 
   s3_target {
     path = "s3://${aws_s3_bucket.incoming_data_bucket.bucket}"
